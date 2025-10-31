91056473cac9c4a0383972f983017b9f45186f1c
Abu Ubaida <ubaidanazia@gmail.com>
2025-10-31 12:13:29 +0600

    Icons File added

---
+++ lib/util/images.dart
@@ -0,0 +1,10 @@
+
+class Images {
+  static const String homeIconActive = 'assets/icons/ic_home_active.png';
+  static const String homeIconInActive = 'assets/icons/ic_home_inactive.png';
+  static const String clipBoardIconActive = 'assets/icons/ic_clipboard_active.png';
+  static const String clipBoardIconInActive = 'assets/icons/ic_clipboard_inactive.png';
+  static const String calenderIconActive = 'assets/icons/ic_calender_active.png';
+  static const String calenderIconInActive = 'assets/icons/ic_clipboard_inactive.png';
+
+}
